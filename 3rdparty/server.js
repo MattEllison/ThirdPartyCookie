53eed2de37c61ca4f62c43a3af05e0199f5278aa
Matthew Ellison <mellison@doctorlogic.com>
2019-02-14 09:54:21 -0600

    setup id

--- 3rdparty/server.js
+++ 3rdparty/server.js
@@ -3,17 +3,26 @@ var fs = require('fs');
 //var index = fs.readFileSync('index.html');
 console.log('starting')
 var refs = [];
+var userCount = 0;
 http.createServer(function (req, res) {
     var ref = req.headers.referer;
-    console.log(ref)
-    refs.push(ref);
-    refs = [...new Set(refs)];
-
-    res.writeHead(200, {
+    console.log(req.headers.cookie)
+    //refs.push(ref);
+    //refs = [...new Set(refs)];
+    var headers = {
         'Content-Type': 'text/plain',
-        'Set-Cookie': `Caller=${refs.join()}`,
         "Access-Control-Allow-Origin": ref.substr(00, ref.length - 1),
         'Access-Control-Allow-Credentials': true,
-    });
-    res.end(refs.join())
+    };
+
+    if (!req.headers.cookie) {
+        userCount++
+        console.log('setting cookie')
+        headers['Set-Cookie'] = `Caller=${userCount}`;
+    }
+    res.writeHead(200, headers);
+
+    res.end(`has cookie ${req.headers.cookie}`)
+
+
 }).listen(9615);
